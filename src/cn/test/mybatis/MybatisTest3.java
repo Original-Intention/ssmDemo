59a33decd0e263fffc386679270dbb2659c8e68a
Original-Intention <851027263@qq.com>
2019-03-26 17:55:15 +0800

    add batchupdate method

--- src/cn/test/mybatis/MybatisTest3.java
+++ src/cn/test/mybatis/MybatisTest3.java
@@ -2,6 +2,7 @@ package cn.test.mybatis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -11,10 +12,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-public class MybatisTest2 {
+public class MybatisTest3 {
 
     @Test
-    public void test() throws IOException { 
+    public void insertUsers() throws IOException { 
     	//Mapper接口开发方法只需要程序员编写Mapper接口（相当于Dao接口），由Mybatis框架根据接口定义创建接口的动态代理对象。
         String resource="cn/test/mybatis/mybatis-config.xml"; 
         //加配置文件
@@ -25,8 +26,18 @@ public class MybatisTest2 {
         //开发中使用这种方式来进行开发，简便快捷，代码复用性高，免去很多重复繁琐代码。
         try {
         	UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
-        	User user=userMapper.findUserById(10);
-        	System.out.println(user);
+        	List<User> users = new ArrayList<User>();
+        	for(int i=0;i<5;i++) {
+        		User user=new User();
+        		user.setUsername("测试"+i);
+        		user.setBirthday(new Date()); 
+        		user.setSex("男");
+        		user.setAddress("测试花果山"+i);
+        		users.add(user);
+        		//userMapper.insertUser(user);        		
+        	}
+        	userMapper.insertUsers(users);
+        	sqlSession.commit();
         }finally {
         	sqlSession.close();
         }
--- src/cn/test/mybatis/UserMapper.java
+++ src/cn/test/mybatis/UserMapper.java
@@ -3,12 +3,36 @@ package cn.test.mybatis;
 import java.util.List;
 
 public interface UserMapper {
-
+	/**
+	 * @author zwh
+	 * @param id 主键
+	 * @date 2019-3-26 16:22:03
+	 * @return 单个用户信息
+	 */
 	public User findUserById(int id);
-
+	/**
+	 * @author zwh
+	 * @param username 用户名
+	 * @date 2019-3-26 16:22:03
+	 * @return 用户信息集合
+	 */
 	public List<User> findUserByUsername(String username);
-
+	/**
+	 * @author zwh
+	 * @date 2019-3-26 16:22:03
+	 * @param user 用户信息
+	 */
 	public void insertUser(User user);
-
+	/**
+	 * @author zwh
+	 * @date 2019-3-26 16:22:03
+	 * @param users 用户信息集合
+	 */
+	public void insertUsers(List<User> users);
+	/**
+	 * @author zwh
+	 * @date 2019-3-26 16:22:03
+	 * @param id 主键
+	 */
 	public void deleteUser(Integer id);
 }
